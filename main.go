6dcc4d9beddc236dc60f145fbfbad03e940fd4d6
chadaevpavel <chadaev.pavel@ya.ru>
2023-08-06 23:31:47 +0600

    parser

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Data struct {
 	str      [3]string
 	operands [2]int
-	operator int // 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление
+	operator string
+	is_roman bool
 }
 
 func main() {
@@ -53,7 +55,54 @@ func reader(data *Data) error {
 }
 
 func parser(data *Data) error {
+	romans := map[int]string{
+		1:  "I",
+		2:  "II",
+		3:  "III",
+		4:  "IV",
+		5:  "V",
+		6:  "VI",
+		7:  "VII",
+		8:  "VII",
+		9:  "IX",
+		10: "X",
+	}
+	operators := []string{"+", "-", "*", "/"}
+	var err1, err2 error
+	var found, found1, found2 bool
 
-	//
+	data.operands[0], err1 = strconv.Atoi(data.str[0])
+	data.operands[1], err2 = strconv.Atoi(data.str[2])
+	if err1 != nil || err2 != nil {
+		for i, literal := range romans {
+			data.str[0] = strings.ToUpper(data.str[0])
+			data.str[2] = strings.ToUpper(data.str[2])
+			if data.str[0] == literal {
+				data.operands[0] = i
+				found1 = true
+			}
+			if data.str[2] == literal {
+				data.operands[1] = i
+				found2 = true
+			}
+		}
+		if found1 && found2 {
+			data.is_roman = true
+		} else if !(err1 != nil && err2 != nil) && (found1 || found2) {
+			return errors.New("Ошибка: нельзя смешивать арабские и римские цифры")
+		} else {
+			return errors.New("Ошибка: неверный формат операндов")
+		}
+	}
+
+	for _, op := range operators {
+		if data.str[1] == op {
+			found = true
+		}
+	}
+
+	if !found {
+		return errors.New("Ошибка: неверный оператор")
+	}
 	return nil
 }
